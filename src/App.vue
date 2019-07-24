188b528c441b7e8df9f23fc0eb910905c7f78dea
ss7424Refar <812647742@qq.com>
2019-07-24 21:06:06 +0800

    [Add] form init data_20190724

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,9 @@
           <side-bar></side-bar>
         </b-col>
         <b-col md="11">
-          <router-view/>
+          <transition name="fade">
+            <router-view/>
+          </transition>
         </b-col>
       </b-row>
     </b-container-fluid>
@@ -28,4 +30,10 @@
 </script>
 
 <style>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,91 +1,93 @@
 <template>
   <div id="main">
     <h3><b-badge variant="dark">查询条件</b-badge></h3>
-    <b-row>
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="资产编号:">
-          <b-input-group>
-            <b-form-input v-model="filter"></b-form-input>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-row>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="资产编号:">
+            <b-input-group>
+              <b-form-input v-model="form.fixed"></b-form-input>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
 
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="资产名称:">
-          <b-input-group>
-            <b-form-input v-model="filter"></b-form-input>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="资产序列号:">
-          <b-input-group>
-            <b-form-input v-model="filter"></b-form-input>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="型号:">
-          <b-input-group>
-            <b-form-input v-model="filter"></b-form-input>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="资产名称:">
+            <b-input-group>
+              <b-form-input v-model="form.names"></b-form-input>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="资产序列号:">
+            <b-input-group>
+              <b-form-input v-model="form.serial"></b-form-input>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="型号:">
+            <b-input-group>
+              <b-form-input v-model="form.type"></b-form-input>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
 
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="使用者:">
-          <b-input-group>
-            <b-form-input v-model="filter"></b-form-input>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="位置:">
-          <b-input-group>
-            <b-form-input v-model="filter"></b-form-input>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="状态:">
-          <b-input-group>
-            <b-form-select v-model="statusSelected" :options="form.statusOptions"></b-form-select>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="使用者:">
+            <b-input-group>
+              <b-form-input v-model="form.user"></b-form-input>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="位置:">
+            <b-input-group>
+              <b-form-input v-model="form.location"></b-form-input>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="状态:">
+            <b-input-group>
+              <b-form-select v-model="form.status" :options="statusOptions"></b-form-select>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
 
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="部门:">
-          <b-input-group>
-            <b-form-select v-model="departSelected" :options="form.departOptions"></b-form-select>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-      <b-col md="4">
-        <b-form-group label-cols-sm="2" label="课:">
-          <b-input-group>
-            <b-form-select v-model="sectionSelected" :options="form.sectionOptions"></b-form-select>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col offset-md="4">
-        <b-button variant="info">查询</b-button>
-      </b-col>
-      <b-col md="7">
-        <b-button variant="primary">重置</b-button>
-      </b-col>
-    </b-row>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="部门:">
+            <b-input-group>
+              <b-form-select v-model="form.depart" :options="departOptions"></b-form-select>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+        <b-col md="4">
+          <b-form-group label-cols-sm="2" label="课:">
+            <b-input-group>
+              <b-form-select v-model="form.section" :options="sectionOptions"></b-form-select>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col offset-md="4">
+          <b-button variant="info" type="submit">查询</b-button>
+        </b-col>
+        <b-col md="7">
+          <b-button variant="primary" type="reset">重置</b-button>
+        </b-col>
+      </b-row>
+    </b-form>
 
     <hr>
 
     <h3><b-badge variant="dark">查询结果</b-badge></h3>
-    <BootstrapTable :columns="columns" :options="options"></BootstrapTable>
+    <BootstrapTable ref="table" :columns="columns" :options="options"></BootstrapTable>
   </div>
 </template>
 
@@ -107,42 +109,48 @@
     // },
     data () {
       return {
-        statusSelected: null,
-        departSelected: null,
-        sectionSelected: null,
         form: {
-          statusOptions: [
-            { value: null, text: '请选择' },
-            { value: '0', text: '在库' },
-            { value: '1', text: '待借出审批' },
-            { value: '2', text: '审核通过' },
-            { value: '3', text: '使用中' },
-            { value: '4', text: '待报废审批' },
-            { value: '5', text: '报废' },
-            { value: '6', text: '待删除审批' },
-          ],
-          departOptions: [
-            { value: null, text: '请选择' },
-            { value: '1884', text: 'SCD' },
-            { value: '2271', text: 'SWV' },
-            { value: '2272', text: 'PSD' },
-            { value: '2273', text: 'CUD' },
-            { value: '2274', text: 'FWD' },
-            { value: '442', text: 'SYD' },
-            { value: '462', text: 'HWD' },
-            { value: '485', text: 'MED' },
-            { value: '491', text: 'CSV' },
-            { value: '499', text: 'HWV' },
-            { value: '520', text: 'PAV' },
-            { value: '540', text: 'SSD' },
-          ],
-          sectionOptions: [
-            { value: null, text: '请选择' },
-            { value: '29', text: 'DT部' },
-            { value: '33', text: 'VT部' },
-            { value: '37', text: 'SWT部' },
-          ]
+          fixed: null,
+          names: null,
+          serial: null,
+          type: null,
+          user: null,
+          location: null,
+          status: null,
+          depart: null,
+          section: null
         },
+        statusOptions: [
+          { value: null, text: '请选择' },
+          { value: '0', text: '在库' },
+          { value: '1', text: '待借出审批' },
+          { value: '2', text: '审核通过' },
+          { value: '3', text: '使用中' },
+          { value: '4', text: '待报废审批' },
+          { value: '5', text: '报废' },
+          { value: '6', text: '待删除审批' },
+        ],
+        departOptions: [
+          { value: null, text: '请选择' },
+          { value: '1884', text: 'SCD' },
+          { value: '2271', text: 'SWV' },
+          { value: '2272', text: 'PSD' },
+          { value: '2273', text: 'CUD' },
+          { value: '2274', text: 'FWD' },
+          { value: '442', text: 'SYD' },
+          { value: '462', text: 'HWD' },
+          { value: '485', text: 'MED' },
+          { value: '491', text: 'CSV' },
+          { value: '499', text: 'HWV' },
+          { value: '520', text: 'PAV' },
+          { value: '540', text: 'SSD' },
+        ],
+        sectionOptions: [
+          { value: null, text: '请选择' },
+          { value: '29', text: 'DT部' },
+          { value: '33', text: 'VT部' },
+          { value: '37', text: 'SWT部' },
+        ],
         columns: [
           {
             field: 'fixed_no',
@@ -180,9 +188,51 @@
           pagination: 'true',
           pageNumber:1,
           pageSize: 20,
-          pageList: [10, 25, 50]
+          pageList: [10, 25, 50],
+          queryParams: function (params) {
+            return params
+          }
         }
       }
-    }
+    },
+    methods: {
+      onSubmit: function (evt) {
+        evt.preventDefault()
+        console.log(JSON.stringify(this.form))
+        let queryParams = function (params) {
+          let temp = {
+            pageSize : params.pageSize,
+            pageNumber : params.pageNumber,
+            formData: JSON.stringify(this.form)
+          };
+        }
+        this.options.queryParams(queryParams);
+
+        // 发送请求
+          this.$http({
+            method:'post',
+            url:'http://localhost/ats-rebuild/services/MachineSever/getSearchData'
+          }).then((response) =>{
+            //这里使用了ES6的语法
+            console.log(response.data);
+            this.data = response.data;
+            //请求成功返回的数据
+          }).catch((error) =>{
+            //请求失败返回的数据
+            console.log(error);
+          })
+      },
+      onReset: function (evt) {
+        this.form.fixed = null
+        this.form.names = null
+        this.form.serial = null
+        this.form.type = null
+        this.form.user = null
+        this.form.location = null
+        this.form.status = null
+        this.form.depart = null
+        this.form.section = null
+      }
+  }
   }
 </script>
